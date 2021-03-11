12e5f42045cfd4d6ee1a7edd57f6e0e1e5b2a8d0
Arun Mopare <arunmopare546@gmail.com>
2021-03-11 20:55:02 +0530

    dummy data added

---
+++ lib/screens/products_overview_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import '../models/products.dart';
+
+class ProductOverviewScreen extends StatelessWidget {
+  final List<Product> loadedProducts;
+  const ProductOverviewScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
