5bcad9ac775616e571aeeea48c83f14df173e3fe
Arun Mopare <arunmopare546@gmail.com>
2021-03-21 21:02:25 +0530

    bug fixes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -1,11 +1,19 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import 'package:my_shop/providers/orders.dart' as ord;
 
-class OrderItem extends StatelessWidget {
+class OrderItem extends StatefulWidget {
   final ord.OrderItem order;
   OrderItem(this.order);
 
+  @override
+  _OrderItemState createState() => _OrderItemState();
+}
+
+class _OrderItemState extends State<OrderItem> {
+  var _expanded = false;
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -14,17 +22,53 @@ class OrderItem extends StatelessWidget {
         children: <Widget>[
           ListTile(
             title: Text(
-              '\$ ${order.amount}',
+              '\$ ${widget.order.amount}',
             ),
             subtitle: Text(
               DateFormat('dd/MM/yyyy hh:mm')
-                  .format(order.dateTime),
+                  .format(widget.order.dateTime),
             ),
             trailing: IconButton(
-              icon: Icon(Icons.arrow_drop_down),
-              onPressed: () {},
+              icon: Icon(_expanded
+                  ? Icons.expand_less
+                  : Icons.expand_more),
+              onPressed: () {
+                setState(() {
+                  _expanded = !_expanded;
+                });
+              },
             ),
-          )
+          ),
+          if (_expanded)
+            Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: 15, vertical: 1),
+              height: min(
+                  widget.order.products.length * 20.0 + 10,
+                  100),
+              child: ListView(
+                children: widget.order.products
+                    .map((prod) => Row(
+                          mainAxisAlignment:
+                              MainAxisAlignment
+                                  .spaceBetween,
+                          children: <Widget>[
+                            Text(
+                              prod.title,
+                              style:
+                                  TextStyle(fontSize: 18),
+                            ),
+                            Text(
+                              '${prod.quantity}x \$${prod.price}',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey),
+                            ),
+                          ],
+                        ))
+                    .toList(),
+              ),
+            )
         ],
       ),
     );
